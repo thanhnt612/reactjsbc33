79505bfa7d136741738d9e76e2123a897017e78f
thanhnt <nguyentruongthanh0612@gmail.com>
2022-09-10 16:22:07 +0700

    setup

---
+++ src/Components/DemoFuncCom.jsx
@@ -0,0 +1,14 @@
+//react class component : rcc
+import React, { Component } from "react";
+
+export default class DemoFuncCom extends Component {
+  //Phuong7 thức chứa giao diện <DemoFuncCom>
+  render() {
+    //Nội dung sẽ chứa giao diện trong lệnh return
+    return (
+      <div>
+        <header>Header component !!!</header>
+      </div>
+    );
+  }
+}
